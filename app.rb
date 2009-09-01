4e1874eb3f5046fcf8790c8dd9e4a2fa5d0dc0cb
Mikael Forsberg <mikaelfor@gmail.com>
2009-09-01 13:43:14 +0200

    added app engine support

--- app.rb
+++ app.rb
@@ -1,14 +1,22 @@
 require 'rubygems'
 require 'sinatra'
-require 'open-uri'
 require 'hpricot'
-
+require 'json'
 require 'java'
 
 get '/' do
-  "Hello from Sinatra running on Java!"
+  url = java.net.URL.new("http://athega.se")
+  ruby_io = org.jruby.RubyIO.new(JRuby.runtime, url.openStream)
+  io = Java.java_to_ruby(ruby_io.java_object)
 
-  doc = Hpricot(open('http://athega.se'))
+  doc = Hpricot(open("http://athega.se"))
+  result = doc/"//*[@id='helplist']/li/a"
 
+  articles = result.collect do | article |
+     {'title' => article.inner_html.strip,
+      'link' => article.attributes['href'] }
+  end
+  content_type :json
+  JSON.generate({'news' => articles})
 end
 
